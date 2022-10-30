22f91b61116f1329a34848587c56d964d2998703
HsuTing <hsuting0106@gmail.com>
2022-10-30 00:51:46 +0000

    feat(zsh): add code alias

--- root/.oh-my-zsh/custom/main.zsh
+++ root/.oh-my-zsh/custom/main.zsh
@@ -1,3 +1,6 @@
+# code
+alias code="code-server --goto"
+
 # node
 alias dnode="ddocker run -it --rm \"node:<DOCKER_NODE_VERSION|lts-alpine>\""
 alias dyarn="ddocker run -it --rm \"node:<DOCKER_YARN_VERSION|DOCKER_NODE_VERSION|lts-alpine>\" yarn"
